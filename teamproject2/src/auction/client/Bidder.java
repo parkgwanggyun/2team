5fe120e456959c454554e94d69ab9a39c2a4ff8b
chanwoohi <chanwoohi@naver.com>
2024-07-15 17:19:28 +0900

    클라이언트 ip port 입력

--- teamproject2/src/auction/client/Bidder.java
+++ teamproject2/src/auction/client/Bidder.java
@@ -10,14 +10,14 @@ public class Bidder {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-//		System.out.print("접속할 IP와 port번호 입력 : ");
-//		String ip = scan.next();
-//		int port = scan.nextInt();
+		System.out.print("접속할 IP와 port번호 입력 : ");
+		String ip = scan.next();
+		int port = scan.nextInt();
 		System.out.print("아이디 입력 : ");
 		String id = scan.next();
 		try {
 			
-			Socket socket = new Socket("192.168.30.209", 6006);
+			Socket socket = new Socket(ip, port);
 			System.out.println("[경매 서버에 연결]");
 			Client client = new Client(socket, id);
 			client.start();
