cbc3131c7d3f3fa2d7f5f95d3125a1b0d300fe7a
kcl0625 <67114778+kcl0625@users.noreply.github.com>
2024-07-17 16:55:31 +0900

    지금까지 한 것 업로드

---
+++ auction/Member.java
@@ -0,0 +1,38 @@
+package auction;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import account.v2.Item;
+import lombok.Data;
+
+@Data
+public class Member implements Serializable {
+	private static final long serialVersionUID = -7070308425216151272L;
+	private String id;
+	private String pw;
+	private List<Item> list = new ArrayList<Item>();
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		return Objects.equals(id, other.id) && Objects.equals(pw, other.pw);
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, pw);
+	}
+	
+	public Member(String id, String pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+}
